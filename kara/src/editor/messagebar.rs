cbeca7b54af4c261aa4f76ea5378dec1bfddc82c
Di P <popov.dmitriiy@gmail.com>
2026-09-13 18:02:29 +0200

    kara: added message expiration

--- kara/src/editor/messagebar.rs
+++ kara/src/editor/messagebar.rs
@@ -1,25 +1,76 @@
-use super::terminal::Terminal;
+use std::{
+    fmt::Display,
+    time::{Duration, Instant},
+};
+
+use crate::editor::terminal::Terminal;
+
+const MESSAGE_DURATION: Duration = Duration::new(5, 0);
+
+struct Message {
+    content: String,
+    time: Instant,
+}
+
+impl Message {
+    fn new(content: String) -> Self {
+        Self {
+            content,
+            time: Instant::now(),
+        }
+    }
+
+    fn is_expired(&self) -> bool {
+        Instant::now().duration_since(self.time) > MESSAGE_DURATION
+    }
+}
+
+impl Default for Message {
+    fn default() -> Self {
+        Self {
+            content: String::new(),
+            time: Instant::now(),
+        }
+    }
+}
+
+impl Display for Message {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.is_expired() {
+            write!(f, "")
+        } else {
+            write!(f, "{}", self.content)
+        }
+    }
+}
 
 #[derive(Default)]
 pub struct MessageBar {
-    current_message: String,
+    current_message: Message,
     needs_redraw: bool,
+    cleared_after_expiration: bool,
 }
 
 impl MessageBar {
     pub fn update_message(&mut self, new_message: String) {
-        if new_message != self.current_message {
-            self.current_message = new_message;
-            self.mark_redraw(true);
-        }
+        self.current_message = Message::new(new_message);
+        self.mark_redraw(true);
+        self.cleared_after_expiration = false;
     }
 
     pub fn render(&mut self, position_y: usize) {
-        if !self.needs_redraw {
+        if !self.needs_render() {
             return;
+        }
+        if self.current_message.is_expired() {
+            self.cleared_after_expiration = true;
         }
         _ = Terminal::print_row(position_y, &self.current_message);
         self.mark_redraw(false);
+    }
+
+    fn needs_render(&self) -> bool {
+        self.needs_redraw || (!self.cleared_after_expiration && self.current_message.is_expired())
     }
 
     const fn mark_redraw(&mut self, value: bool) {
